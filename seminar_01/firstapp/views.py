4aebee0e0a1c3eaa9e7846906dea7eb6ec811e64
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-08-16 20:45:54 +0300

    Add seminar 3

--- seminar_01/firstapp/views.py
+++ seminar_01/firstapp/views.py
@@ -7,65 +7,17 @@ from .models import Author, Article, Commentary
 logger = logging.getLogger(__name__)
 
 
-# def index_old(request):
-#     logger.info(f'Index accessed')
-#     html = """<html>
-#     <head><title>Index page</title></head>
-#     <body>
-#     <div><h2>Site Start Page</h2></div>
-#         <div>
-#             <ul>Simple Games
-#                 <li><a href="games">Games Page</a></li>
-#             </ul>
-#         </div>
-#         <footer>
-#         <a href="">Main</a>&nbsp;&nbsp;&nbsp;
-#         <a href="about/">About</a>
-#         </footer>
-#     </body>
-#     </html>
-#     """
-#     return HttpResponse(html)
-#
-#
-# def about_old(request):
-#     logger.info(f'About accessed')
-#     html = """<html>
-#     <head><title>About page</title></head>
-#     <body>
-#     <div><h2>Site About Page</h2></div>
-#          <div>
-#           <p><strong>Lorem ipsum</strong> dolor sit amet, consectetur adipisicing elit.
-#           Ab amet blanditiis dolores laudantium maiores, nulla quas
-#           repudiandae saepe. A aspernatur consectetur dolore est illo nam
-#           necessitatibus nostrum nulla numquam tempora! Culpa delectus dolorem,
-#           dolorum ea eos eveniet facilis fugiat illo illum in ipsum minima
-#           neque odit perferendis porro quaerat quia quibusdam quo quod
-#           reiciendis repudiandae saepe similique, temporibus vel
-#           voluptate. Aut beatae doloremque fugiat laboriosam magni,
-#           optio qui quia recusandae ut veritatis. Commodi, culpa dignissimos
-#           doloribus est et illo, ipsa laboriosam laborum molestias natus nemo
-#           odit officiis placeat sequi sunt veniam, voluptas!
-#           At ea ex odit placeat sunt? Assumenda, tenetur!</p>
-#         </div>
-#         <footer>
-#         <a href="/">Main</a>&nbsp;&nbsp;&nbsp;
-#         <a href="">About</a>
-#         </footer>
-#     </body>
-#     </html>
-#     """
-#     return HttpResponse(html)
+
 def index(request):
     logger.info(f'Index accessed')
     context = {"title": "Index page"}
-    return render(request, "app001/index.html", context=context)
+    return render(request, "firstapp/index.html", context=context)
 
 
 def about(request):
     logger.info(f'About accessed')
     context = {"title": "About page"}
-    return render(request, "app001/about.html", context=context)
+    return render(request, "firstapp/about.html", context=context)
 
 
 # =========================================================================
@@ -84,7 +36,7 @@ def articles(request, author_id):
         "author": f'{author.name} {author.surname}',
         "articles": list(Article.objects.filter(author=author))
     }
-    return render(request, 'app001/authors_records.html', context=context)
+    return render(request, 'firstapp/authors_records.html', context=context)
 
 
 # =========================================================================
@@ -105,7 +57,7 @@ def get_article(request, article_id):
         'author': author,
         'commentaries': commentaries,
     }
-    return render(request, 'app001/article.html', context=context)
+    return render(request, 'firstapp/article.html', context=context)
 
 # =========================================================================
 # Измените шаблон для вывода заголовка и текста статьи, а также всех
